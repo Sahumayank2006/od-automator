9956e0a9405f0db93d064865f92f458a7090ae7b
mayank sahu <mayank.sahu.2006.ms@gmail.com>
2025-08-15 20:28:03 +0000

    add the variable of databse to env file so i can pste from there

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "od-nimbus",
-  "appId": "1:633930719463:web:87b969c74d0644c7ee8242",
-  "storageBucket": "od-nimbus.firebasestorage.app",
-  "apiKey": "AIzaSyCB9c2DODy4ol9cesXaeTb3hI4fsaxpXlM",
-  "authDomain": "od-nimbus.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "633930719463"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
